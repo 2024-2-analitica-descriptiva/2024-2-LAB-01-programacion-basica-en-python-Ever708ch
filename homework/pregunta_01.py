0de0ed26ca6e4e81ddc82ee11d1f18a3b2b8eb27
Ever Herrera Ríos <evherrera@unal.edu.co>
2024-11-20 20:00:22 -0500

    1er intento

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,9 +5,16 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+x = open(r"D:\ever\PhD\descriptiva\2024-2-LAB-01-programacion-basica-en-python-Ever708ch\files\input\data.csv").readlines()
+def pregunta_01(x):
+    x = [i.split("\t") for i in x]
+    x = [int(i[1]) for i in x]
+    x = sum(x)
+    return x
+resultado = pregunta_01(x)
+print(resultado)
 
-def pregunta_01():
-    """
+"""
     Retorne la suma de la segunda columna.
 
     Rta/
